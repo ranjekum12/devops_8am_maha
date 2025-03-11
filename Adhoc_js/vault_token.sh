ccdce8cdb2e32496c73442af690633743035bf1f
ranjekum12 <160642209+ranjekum12@users.noreply.github.com>
2025-03-11 10:22:41 +0530

    Delete Adhoc_js directory
    mmm

--- Adhoc_js/vault_token.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-set -e
-export VAULT_TOKEN=$(aws secretsmanager get-secret-value --secret-id arn:aws:secretsmanager:eu-west-1:483013340174:secret:campaign/provisioning/vault_token-zsAW8Q  --query SecretString --output text  --region eu-west-1 |  tr { '\n' | tr , '\n' | tr } '\n' | grep 'VAULT_TOKEN' | awk  -F'"' '{print $4}')
